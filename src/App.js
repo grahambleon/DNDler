5d95fd914cab1df0c8799969b7e44601aedfb39f
Graham Gilpatric <grahamwithag@gmail.com>
2019-03-27 01:15:37 -0400

    added milonga font

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const StyledTitle = styled.div`
+const Title = styled.div`
   text-align: center;
+  margin: 10px;
+  font-family: 'Milonga', cursive;
+  font-size: 36px;
+`
+const Instructions = styled.div`
+  text-align: center;
+  margin: 10px;
 `
 
 class App extends Component {
   render() {
     return (
-      <StyledTitle>
-        WELCOME TO THE DNDLER
-      </StyledTitle>
+      <>
+        <Title>
+          WELCOME TO THE DNDLER
+        </Title>
+        <Instructions>
+          -How to use the DNDler
+        </Instructions>
+      </>
     );
   }
 }
