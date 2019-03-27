165581f108d425b42e4471baa55293a16711dd18
Graham Gilpatric <grahamwithag@gmail.com>
2019-03-27 03:10:47 -0400

    added smoke signal test for ButtonContainer

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import App from './App';
+
+it('renders', () => {
+  shallow(<App />)
+})
 
 it('renders correctly', () => {
   const tree = renderer
---
+++ src/containers/ButtonContainer.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ButtonContainer from './ButtonContainer.js'
+
+it('renders', () => {
+  shallow(<ButtonContainer />)
+})
