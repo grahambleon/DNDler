0682920b526c912e95d0e8c8dbe3d471a5478bdf
Graham Gilpatric <grahamwithag@gmail.com>
2019-03-27 02:18:06 -0400

    initial snapshot setup

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
