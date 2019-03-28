81de5c53907f281f3918aeb5259c2ee395c8e28c
Graham Gilpatric <grahamwithag@gmail.com>
2019-03-27 23:31:18 -0400

    updated snapshot, lodash.sample dependency added, basic start to generator

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+const sample = require('lodash.sample');
 
 const StyledSheet = styled.div`
   text-align: center;
@@ -7,23 +8,35 @@ const StyledSheet = styled.div`
 `
 
 class CharacterSheet extends Component {
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //
-  //   }
-  // }
-  // componentDidMount() {
-  //
-  // }
+  constructor(props){
+    super(props)
+    this.state = {
+      race: '',
+      class: '',
+      background: ''
+    }
+    this.rollCharacter = this.rollCharacter.bind(this)
+  }
+
+  componentDidMount() {
+    this.rollCharacter()
+  }
 
   rollCharacter() {
+    const races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling','Half-Orc', 'Human', 'Tiefling']
+    const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk','Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+    const backgrounds = ['Acolyte', 'Charlatan', 'Criminal/Spy', 'Entertainer','Folk Hero', 'Gladiator', 'Guild Artisan/Guild Merchant', 'Hermit', 'Knight', 'Noble', 'Outlander', 'Pirate', 'Sage', 'Sailor', 'Soldier', 'Urchin']
 
+    this.setState({
+      race: sample(races),
+      class: sample(classes),
+      background: sample(backgrounds)
+    })
   }
 
   render() {
     return(
-      <StyledSheet>[DNDled Character]</StyledSheet>
+      <StyledSheet>{this.state.race}, {this.state.class}, {this.state.background}</StyledSheet>
     )
   }
 }
